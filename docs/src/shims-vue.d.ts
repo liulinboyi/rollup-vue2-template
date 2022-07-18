b4636bec516c30513a7fd4a477b3889de4fdf834
liulinboyi <814921718@qq.com>
2022-07-18 15:37:55 +0800

    fix: 修复示例类型错误

--- docs/src/shims-vue.d.ts
+++ docs/src/shims-vue.d.ts
@@ -1,4 +1,8 @@
-declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
+declare module "*.vue" {
+  import Vue from "vue";
+  export default {
+    ...Vue,
+    install: any,
+    name: any,
+  };
 }
